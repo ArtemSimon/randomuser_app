a4bf1f726a7616bb07d6099692f83cdfc176dec4
artem <artemsim2006@mail.ru>
2025-05-24 12:00:01 +0300

    change func for request on external api for performance and memory savings

--- app/api/service.py
+++ app/api/service.py
@@ -1,29 +1,50 @@
+import asyncio
+import math
+
+from fastapi import Depends
+from app.api.models import User
+from app.database import get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
 import httpx
+from typing import AsyncGenerator, List
+import uuid
 
-async def fetch_random_users(count:int):
-    
-    url = f"https://randomuser.me/api/?results={count}"
-
+"""Асинхронный генератор, который загружает пользователей порциями"""
+async def fetch_random_users(count: int, page: int = 1) -> AsyncGenerator[List[dict], None]:
     async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(url)
-            response.raise_for_status()  # Проверяет, что статус 2xx
-            return response.json()
-        except httpx.HTTPStatusError as exc:
-            print(f"HTTP error occurred: {exc}")
-    
-
-def parse_user(user_data: dict) -> dict:
-    return {
-        "external_id": user_data.get("login", {}).get("uuid", "unknown"),
-        "gender": user_data.get("gender", ""),
-        "first_name": user_data.get("name", {}).get("first", ""),
-        "last_name": user_data.get("name", {}).get("last", ""),
-        "email": user_data.get("email", ""),
-        "phone": user_data.get("phone", ""),
-        "city": user_data.get("location", {}).get("city", ""),
-        "country": user_data.get("location", {}).get("country", ""),
-        "postcode": (user_data["location"]["postcode"]),
-        "thumbnail": user_data.get("picture", {}).get("thumbnail", ""),
-        'profile_url': f"/internal_user/{user_data['login']['uuid']}"
+        params = {
+        "results": count,
+        "page": page
     }
+        
+    async with httpx.AsyncClient() as client:
+        response = await client.get("https://randomuser.me/api/", params=params)
+        return response.json()
+
+"""Парсинг данных API -> User модель"""
+async def parse_user(user_data: dict) -> User:
+    return User(
+        external_id=uuid.UUID(user_data["login"]["uuid"]),
+        gender=user_data["gender"],
+        first_name=user_data["name"]["first"],
+        last_name=user_data["name"]["last"],
+        email=user_data["email"],
+        phone=format_phone(user_data["phone"]),
+        street=f"{user_data['location']['street']['number']} {user_data['location']['street']['name']}",
+        city=user_data["location"]["city"],
+        state=user_data["location"]["state"],
+        country=user_data["location"]["country"],
+        postcode=str(user_data["location"]["postcode"]),
+        picture_thumbnail=user_data["picture"]["thumbnail"],
+        profile_url = f"http://homepage/internal_user/{user_data['login']['uuid']}"
+    )
+
+"""Обработка пакета пользователей"""
+async def process_users_batch(batch: List[dict]) -> List[User]:
+    return await asyncio.gather(*[parse_user(user) for user in batch])
+
+
+"""Форматирование телефонного номера"""
+def format_phone(phone: str) -> str:
+    return "+" + "".join(filter(str.isdigit, phone))
+
