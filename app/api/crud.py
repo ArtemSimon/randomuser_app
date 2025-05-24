f33450ec1a06049b0374b820ec90e96fbfbdd964
artem <artemsim2006@mail.ru>
2025-05-24 11:55:42 +0300

    create func for load_user in external api and create endpoints load user  from db on website and reset for delete all users in db and add html in endpoint for display user

--- app/api/crud.py
+++ app/api/crud.py
@@ -1,6 +1,8 @@
 from fastapi import HTTPException, logger
 from app.api.models import User
-from app.api.service import fetch_random_users,parse_user
+from app.api.service import fetch_random_users,process_users_batch
+# from app.api.service import fetch_random_users,parse_user,process_users_batch,fetch_users
+from uuid import UUID
 from pydantic import BaseModel
 from sqlalchemy import func, select
 from sqlalchemy.exc import SQLAlchemyError
@@ -12,22 +14,58 @@ class UserApi():
         self.session = session
 
     """Асинхронная загрузка пользователей из внешнего API с парсингом """
-    async def async_load_user(self,count:int) -> int:
-
+    async def async_load_user(self, total:int, batch_size:int = 5000) -> int:
+           
         try:
-            users_data = await fetch_random_users(count)
-            users_dict = [parse_user(user_data) for user_data in users_data]
-            self.session.add_all()
-            await self.session.commit()
-            return len(users_dict)
+            added_count = 0
+
+        
+            # Рассчитываем количество страниц
+            total_pages = (total + batch_size - 1) // batch_size 
+            
+            for page in range(1, total_pages + 1):
+                # Определяем количество пользователей для текущей страницы
+                current_batch_size = min(batch_size, total - (page-1)*batch_size)
+                
+                # Загружаем данные с API
+                response = await fetch_random_users(
+                    count=current_batch_size,
+                    page=page,
+                )
+                
+                users = response['results']
+                print(f"Загружено страница {page}: {len(users)} пользователей")
+            
+                for i in range(0, len(users), 100):  # Внутренний batch по 100 записей
+                    small_batch = users[i:i + 100]
+                    parsed_users = await process_users_batch(small_batch)
+                    
+                    for user_data in parsed_users:
+                        # # Проверяем, существует ли пользователь с таким email
+                        # existing_user = await self.session.execute(
+                        #     select(User).where(User.email == user_data.email)
+                        # )
+                        # if existing_user.scalar_one_or_none() is None:
+                        self.session.add(user_data)
+                        await self.session.flush()  # Частичное сохранение
+                        user_data.profile_url = f'http://homepage/internal_user/{user_data.id}'
+                        added_count += 1
+                
+                
+                await self.session.commit() 
+            
+            print(f"Всего загружено пользователей: {added_count}")
+            return added_count
         
         except Exception as e:
             await self.session.rollback()
-            return {'message':f'Ошибка при загрузке пользователей'}
+            print(f"Ошибка при загрузке пользователей: {str(e)}")
             raise HTTPException(
-            status_code=500,
-            detail=f"User loading failed: {str(e)}"
-        )
+                status_code=500,
+                detail=f"User loading failed: {str(e)}"
+            )
+        finally:
+            await self.session.close()
 
     """ Получение конкретного пользователя по id """
     async def get_user_by_id(self,user_id:int) -> User:
@@ -61,3 +99,29 @@ class UserApi():
                 status_code=500,
                 detail="Не удалось получить случайного пользователя."
             )
+        
+
+    async def get_users_from_db(self,limit:int, offset:int = 0):
+        try:
+            query= select(User)
+            users_in_db = await self.session.execute(
+                query.order_by(User.id)
+                .limit(limit)
+                .offset(offset)
+            )
+
+            users_final = users_in_db.scalars().all()
+
+            count_query = select(func.count()).select_from(User)
+
+
+            total_all = (await self.session.execute(count_query)).scalar_one()
+
+            return users_final, total_all
+        
+        except SQLAlchemyError as e:
+            logger.error(f"Database error: {str(e)}")
+            raise HTTPException(
+            status_code=500,
+            detail="Database error"
+        )
--- app/api/router.py
+++ app/api/router.py
@@ -1,18 +1,44 @@
 
+import logging
+
+from sqlalchemy import delete, select, text
 from app.api.schemas import SUserResponse, SUserFind
 from app.api.crud import UserApi
+from app.api.models import User
 from app.database import get_async_session
-from fastapi import APIRouter,Depends, Query
+from fastapi import APIRouter,Depends, HTTPException, Query, logger,status,Request
+from fastapi.templating import Jinja2Templates
+from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.api.service import fetch_random_users,parse_user
+
 
 router = APIRouter()
 
-
+logger = logging.getLogger(__name__)
+templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/health")  # для проверки работоспособности сервера 
 async def health_check():
     return {"status": "ok"}
+
+@router.get('/users_in_db')
+async def get_users(
+    page: int,
+    limit: int,
+    session: AsyncSession = Depends(get_async_session)
+):
+    user = UserApi(session)
+    users_get,count = await user.get_users_from_db(limit,offset=(page-1)*limit)
+    return {
+        "data": users_get,
+        "meta": {
+            "total": count,
+            "page": page,
+            "per_page": limit,
+            "total_pages": (count + limit - 1) // limit
+        }
+    }
+
 
 @router.get('/load_user')
 async def load_user_random(
@@ -20,27 +46,59 @@ async def load_user_random(
     session: AsyncSession = Depends(get_async_session),
 ):
     users_api = UserApi(session)
+    users_count = await users_api.async_load_user(count)
     if not users_api:
         return {'message':f'Пользователи не загрузились'}
-    return await users_api.async_load_user(count)
+    return users_count
+
+
+@router.delete("/reset/", summary="Удалить всех пользователей и сбросить ID")
+async def reset_users (
+    session: AsyncSession = Depends(get_async_session)
+):
+    try:
+        # Удаляем всех пользователей
+        await session.execute(delete(User))
+        
+        # Сбрасываем SEQUENCE
+        await session.execute(text("ALTER SEQUENCE users_id_seq RESTART WITH 1;"))
+        await session.commit()
+        
+        return {"message": "Все пользователи удалены, ID сброшен."}
+    except Exception as e:
+        await session.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 @router.get('/internal_user/{user_id}',response_model=SUserResponse,summary="Получение пользователя по id")
 async def get_user(
     user_id: int,
+    request: Request,
     session: AsyncSession = Depends(get_async_session)
 ):
     user = UserApi(session)
     data_user = await user.get_user_by_id(user_id)
-    if data_user is None:
-        return {'message':f'Записи с таким  нету'}  # сделать нормальное логирование
-    return data_user
+    return templates.TemplateResponse(
+        "user_profile.html",
+        {
+            "request": request,
+            "user": data_user,
+            "title": f"Профиль {data_user.first_name} {data_user.last_name}"
+        }
+    )
 
 @router.get('/random',response_model=SUserResponse,summary="Получение случайного пользователя")
 async def get_random_user(
+    request: Request,
     session: AsyncSession = Depends(get_async_session)
 ):
     user_random = UserApi(session)
     data_user_random = await user_random.get_random_user()
-    if data_user_random is None:
-        return {'message':f'Записи с таким {data_user_random.id} нету'}
-    return data_user_random
+    return templates.TemplateResponse(
+        "user_profile.html",
+        {
+            "request": request,
+            "user": data_user_random,
+            "title": f"Профиль {data_user_random.first_name} {data_user_random.last_name}"
+        }
+    )
