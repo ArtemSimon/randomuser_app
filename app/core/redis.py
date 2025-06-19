ad8330d19b52cffcf1f925c3f32a8102ddd7059f
artem <artemsim2006@mail.ru>
2025-06-19 21:50:48 +0300

    add redis client

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,19 +1,19 @@
 from redis.asyncio import Redis
-from app.core.config import settings
+from app.config import settings,logger
 
-redis_client = None
+
 
 async def get_redis() -> Redis:
-    global redis_client
-    if redis_client is None:
-        redis_client = Redis.from_url(
-            f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
-            db=settings.REDIS_DB,
-            decode_responses=True
-        )
-    return redis_client
 
-async def close_redis():
-    global redis_client
-    if redis_client:
+    redis_client = Redis.from_url(
+        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+        decode_responses=True # decode_responses=True,
+        # для автоматического декодирования в str
+    )
+    try:
+        await redis_client.ping()
+        return redis_client
+    except Exception as e:
         await redis_client.close()
+        logger.error('Error for connect Redis')
+        raise RuntimeError(f'Error for connect at Redis:{e}')
